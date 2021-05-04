b2af822ef3aff9af2b225cdfdc15079a785bd34b
Oscar Cornejo <oscarcornejo10@gmail.com>
2021-05-03 23:09:39 -0400

    welcome message is updated

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Coder!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/oscarcornejo/haaken"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check the Repository on Github ;)
         </a>
       </header>
     </div>
